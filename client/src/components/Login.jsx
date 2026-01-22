2e2cbd758a4c94d0030358a4e401704e145b4c62
Fahad Daud <Daudf@PHARMEVO.COM>
2026-01-22 10:51:19 +0500

    better start scripts 2

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
             </svg>
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            SFE Chatbot
+            Chatbot
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Knowledge Base Management System
